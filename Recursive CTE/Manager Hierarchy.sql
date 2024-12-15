40162211e54cad211cc0ecf68b9d34ac82dbf4ec
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-12-14 17:50:05 -0800

    Update Manager Hierarchy.sql

--- Recursive CTE/Manager Hierarchy.sql
+++ Recursive CTE/Manager Hierarchy.sql
@@ -1,4 +1,6 @@
 -- Solving problem to find hierarchy/querying hierarchical data 
+
+-- Finding All Employees for Given Mananger
 WITH RECURSIVE emp_hierarchy AS (
     SELECT id, name, manager_id, designation, 1 AS lvl
     FROM emp_details 
@@ -10,3 +12,20 @@ WITH RECURSIVE emp_hierarchy AS (
 )
 SELECT * 
 FROM emp_hierarchy;
+
+-- Finding All Managers for Given Employee 
+WITH RECURSIVE emp_hierarchy AS (
+    SELECT id, name, manager_id, designation, 1 AS lvl
+    FROM emp_details 
+    WHERE name = 'David'
+    UNION
+    SELECT E.id, E.name, E.manager_id, E.designation, H.lvl + 1 AS lvl
+    FROM emp_hierarchy H
+    JOIN emp_details E ON H.manager_id = E.id
+)
+SELECT H2.id AS emp_id, 
+       H2.name AS emp_name, 
+       E2.name AS manager_name, 
+       H2.lvl AS level
+FROM emp_hierarchy H2
+JOIN emp_details E2 ON E2.id = H2.manager_id;
