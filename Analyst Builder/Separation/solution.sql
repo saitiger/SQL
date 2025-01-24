c56bff608a4bb5e834c685af45f7129c941118ec
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2025-01-24 11:44:36 -0800

    Update solution.sql

--- Analyst Builder/Separation/solution.sql
+++ Analyst Builder/Separation/solution.sql
@@ -1,3 +1,11 @@
-SELECT substring(id,1,5) as ID,
-substring(id,6) as First_Name
-FROM bad_data;
+-- Solution 1 
+SELECT substring(id,1,5) ID, substring(id,6) First_Name
+FROM 
+bad_data
+
+-- Solution 2 
+SELECT
+regexp_replace(CombinedColumn, '[^0-9].*', '') ID,  -- Extract numeric prefix as ID
+regexp_replace(CombinedColumn, '^[0-9]+', '') FirstName -- Extract non-numeric suffix as FirstName
+FROM 
+YourTable
